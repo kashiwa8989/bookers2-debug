b049d98de0e370aa0082cb73092b15e106b85f44
kashiwa8989 <119640627+kashiwa8989@users.noreply.github.com>
2023-01-05 13:26:51 +0000

    third commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,11 @@ class User < ApplicationRecord
   # belongs_to :books
   has_many :books
   has_many :favorites, dependent: :destroy
+  has_many :book_comments, dependent: :destroy
   has_one_attached :profile_image
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
-  validates :introduction, length: {maximum: 20}
+  validates :introduction, length: {maximum: 50}
 
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
